d44018a42fa2aae789e67441721ee4392063941a
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-08-31 13:19:00 -0400

    feat(linebreak): port line break engine

---
+++ engine-haxe/src/org/tiqian/linebreak/EnglishHyphenationPatterns.hx
@@ -0,0 +1,10 @@
+package org.tiqian.linebreak;
+
+@:build(DataTables.codeUnitsField("engine-haxe/data/hyph-en-us.tex", "TEX_UNITS"))
+class EnglishHyphenationPatterns {
+    public static function load():String {
+        final output = new std.StringBuf(); var index = 0;
+        while (index < TEX_UNITS.length) { output.add(String.fromCharCode(TEX_UNITS[index])); index++; }
+        return output.toString();
+    }
+}
---
+++ engine-haxe/src/org/tiqian/linebreak/LinebreakTests.hx
@@ -0,0 +1,21 @@
+package org.tiqian.linebreak;
+
+import org.tiqian.test.trace.TestTraceRecorder;
+import org.tiqian.test.trace.TracedAssertions;
+
+class EnglishHyphenationTest {
+    private static function hyphenated(word:String):String { final offsets = EnglishHyphenation.enUs().hyphenate(word); var out = new StringBuf(); var i = 0; while (i < word.length) { var has = false; var oi = 0; while (oi < offsets.length) { if (offsets[oi] == i) has = true; oi++; } if (has) out.add("-"); out.add(word.charAt(i)); i++; } return out.toString(); }
+    public static function hyphenatesCommonWordsAtSyllablePoints():Void { new TestTraceRecorder("EnglishHyphenationTest").section("hyphenatesCommonWordsAtSyllablePoints"); TracedAssertions.assertEqualsString("hy-phen-ation", hyphenated("hyphenation")); TracedAssertions.assertEqualsString("com-puter", hyphenated("computer")); TracedAssertions.assertTrue(hyphenated("international").indexOf("in-ter") == 0, hyphenated("international")); }
+    public static function respectsMarginsAndShortWords():Void { new TestTraceRecorder("EnglishHyphenationTest").section("respectsMarginsAndShortWords"); TracedAssertions.assertEqualsIntArray([], EnglishHyphenation.enUs().hyphenate("the")); TracedAssertions.assertEqualsIntArray([], EnglishHyphenation.enUs().hyphenate("a")); }
+    public static function honoursTheExceptionList():Void { new TestTraceRecorder("EnglishHyphenationTest").section("honoursTheExceptionList"); TracedAssertions.assertEqualsIntArray([], EnglishHyphenation.enUs().hyphenate("project")); TracedAssertions.assertEqualsIntArray([], EnglishHyphenation.enUs().hyphenate("present")); }
+}
+
+class LiangHyphenatorTest {
+    private static function table(keys:Array<String>, values:Array<Array<Int>>):runtime.SortedTable.SortedMapTable<String, Array<Int>> { final b=runtime.SortedTable.mapBuilder(runtime.SortedTable.compareStrings); var i=0; while (i<keys.length) { b.put(keys[i], values[i]); i++; } return b.build(); }
+    public static function noHyphenatorYieldsNoOpportunities():Void { new TestTraceRecorder("LiangHyphenatorTest").section("noHyphenatorYieldsNoOpportunities"); TracedAssertions.assertEqualsIntArray([], new Hyphenator.NoHyphenator().hyphenate("international")); }
+    public static function oddLevelGapBecomesABreakOutsideTheMargins():Void { new TestTraceRecorder("LiangHyphenatorTest").section("oddLevelGapBecomesABreakOutsideTheMargins"); final h = new LiangHyphenator(table([["c", [1,0]]]), null, 1, 1); TracedAssertions.assertEqualsIntArray([2], h.hyphenate("abc")); TracedAssertions.assertEqualsIntArray([], h.hyphenate("cab")); }
+    public static function maxLevelWinsAndEvenForbidsTheBreak():Void { new TestTraceRecorder("LiangHyphenatorTest").section("maxLevelWinsAndEvenForbidsTheBreak"); final h = new LiangHyphenator(table([["ab",[0,1,0]],["zab",[0,0,2,0]]]), null, 1, 1); TracedAssertions.assertEqualsIntArray([1],h.hyphenate("ab")); TracedAssertions.assertEqualsIntArray([],h.hyphenate("zab")); }
+    public static function marginsAndShortWordsAreRespected():Void { new TestTraceRecorder("LiangHyphenatorTest").section("marginsAndShortWordsAreRespected"); final h = new LiangHyphenator(table([["a",[1,0]]]),null,2,3); TracedAssertions.assertEqualsIntArray([],h.hyphenate("the")); }
+    public static function exceptionsOverridePatternsAndAreCaseInsensitive():Void { new TestTraceRecorder("LiangHyphenatorTest").section("exceptionsOverridePatternsAndAreCaseInsensitive"); final e=table([["table",[2]]]); final h=new LiangHyphenator(table([]),e,1,1); TracedAssertions.assertEqualsIntArray([2],h.hyphenate("table")); TracedAssertions.assertEqualsIntArray([2],h.hyphenate("Table")); }
+    public static function parsesPatternsAndExceptionBlocksStrippingComments():Void { new TestTraceRecorder("LiangHyphenatorTest").section("parsesPatternsAndExceptionBlocksStrippingComments"); final p=ParseTexHyphenationPatterns.parse("\\patterns{ .ach4 a5bal }\\hyphenation{ ta-ble present }"); TracedAssertions.assertEqualsIntArray([0,0,0,0,4],p.patterns.get(".ach")); TracedAssertions.assertEqualsIntArray([0,5,0,0,0],p.patterns.get("abal")); TracedAssertions.assertEqualsIntArray([2],p.exceptions.get("table")); TracedAssertions.assertEqualsIntArray([],p.exceptions.get("present")); }
+}
