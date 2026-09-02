a06a45af3f9066ff2818e775a4b77de1f287d222
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-09-02 14:03:33 -0400

    port(tests): geometry ledger tests partial

--- engine-haxe/src/org/tiqian/layout/LineGeometryDirectTailTest.hx
+++ engine-haxe/src/org/tiqian/layout/LineGeometryDirectTailTest.hx
@@ -3,12 +3,12 @@ package org.tiqian.layout;
 import org.tiqian.core.IllegalStateException;
 
 @:test class LineGeometryDirectTailTest {
-    @:test public static function rubyBaseRangeCrossingClusterBoundariesDropsOutOfPerLineExtents():Void throw new IllegalStateException("TODO r7: rubyBaseRangeCrossingClusterBoundariesDropsOutOfPerLineExtents");
-    @:test public static function rubiesOnBothLinesExerciseBothSidesOfTheOverlapTest():Void throw new IllegalStateException("TODO r7: rubiesOnBothLinesExerciseBothSidesOfTheOverlapTest");
-    @:test public static function emptyLineSolutionYieldsZeroArraysAndZeroMaxExtra():Void throw new IllegalStateException("TODO r7: emptyLineSolutionYieldsZeroArraysAndZeroMaxExtra");
-    @:test public static function objectTopIntrusionBelowRubyDemandKeepsBoundaryClearanceZero():Void throw new IllegalStateException("TODO r7: objectTopIntrusionBelowRubyDemandKeepsBoundaryClearanceZero");
-    @:test public static function objectTopIntrusionDominatingRubyDemandAddsBoundaryClearance():Void throw new IllegalStateException("TODO r7: objectTopIntrusionDominatingRubyDemandAddsBoundaryClearance");
-    @:test public static function objectFlushWithBaseTopSkipsIntrusionConjunctionEarly():Void throw new IllegalStateException("TODO r7: objectFlushWithBaseTopSkipsIntrusionConjunctionEarly");
-    @:test public static function metricListWithoutIdeographicEmBoxFallsBackToAllClusters():Void throw new IllegalStateException("TODO r7: metricListWithoutIdeographicEmBoxFallsBackToAllClusters");
-    @:test public static function emptyMetricListTakesEmptyParagraphBaselineFallback():Void throw new IllegalStateException("TODO r7: emptyMetricListTakesEmptyParagraphBaselineFallback");
+    @:test public static function rubyBaseRangeCrossingClusterBoundariesDropsOutOfPerLineExtents():Void throw new IllegalStateException("TODO r8: rubyBaseRangeCrossingClusterBoundariesDropsOutOfPerLineExtents");
+    @:test public static function rubiesOnBothLinesExerciseBothSidesOfTheOverlapTest():Void throw new IllegalStateException("TODO r8: rubiesOnBothLinesExerciseBothSidesOfTheOverlapTest");
+    @:test public static function emptyLineSolutionYieldsZeroArraysAndZeroMaxExtra():Void throw new IllegalStateException("TODO r8: emptyLineSolutionYieldsZeroArraysAndZeroMaxExtra");
+    @:test public static function objectTopIntrusionBelowRubyDemandKeepsBoundaryClearanceZero():Void throw new IllegalStateException("TODO r8: objectTopIntrusionBelowRubyDemandKeepsBoundaryClearanceZero");
+    @:test public static function objectTopIntrusionDominatingRubyDemandAddsBoundaryClearance():Void throw new IllegalStateException("TODO r8: objectTopIntrusionDominatingRubyDemandAddsBoundaryClearance");
+    @:test public static function objectFlushWithBaseTopSkipsIntrusionConjunctionEarly():Void throw new IllegalStateException("TODO r8: objectFlushWithBaseTopSkipsIntrusionConjunctionEarly");
+    @:test public static function metricListWithoutIdeographicEmBoxFallsBackToAllClusters():Void throw new IllegalStateException("TODO r8: metricListWithoutIdeographicEmBoxFallsBackToAllClusters");
+    @:test public static function emptyMetricListTakesEmptyParagraphBaselineFallback():Void throw new IllegalStateException("TODO r8: emptyMetricListTakesEmptyParagraphBaselineFallback");
 }
--- engine-haxe/src/org/tiqian/layout/PunctuationGeometryLedgerCoverageTest.hx
+++ engine-haxe/src/org/tiqian/layout/PunctuationGeometryLedgerCoverageTest.hx
@@ -1,20 +1,22 @@
 package org.tiqian.layout;
+
 import org.tiqian.core.IllegalStateException;
-class PunctuationGeometryLedgerCoverageTest {
-    @:test public static function budgetsResolveAdvancesThroughRemainingGlue():Void throw new IllegalStateException("TODO r7: budgetsResolveAdvancesThroughRemainingGlue");
-    @:test public static function glueCapacitiesReportSidesAndPairing():Void throw new IllegalStateException("TODO r7: glueCapacitiesReportSidesAndPairing");
-    @:test public static function sideConsumptionIsCappedAndSkipsNonPositiveAmounts():Void throw new IllegalStateException("TODO r7: sideConsumptionIsCappedAndSkipsNonPositiveAmounts");
-    @:test public static function justificationDeltasAndStructuralChannelsFeedResolvedAdvance():Void throw new IllegalStateException("TODO r7: justificationDeltasAndStructuralChannelsFeedResolvedAdvance");
-    @:test public static function geometryWithoutBudgetFallsBackToBodyWidth():Void throw new IllegalStateException("TODO r7: geometryWithoutBudgetFallsBackToBodyWidth");
-    @:test public static function decisionInfoListsEveryGeometryWithBudgets():Void throw new IllegalStateException("TODO r7: decisionInfoListsEveryGeometryWithBudgets");
-    @:test public static function spacingPlanAdjustmentsConsumeByTargetAndAnchor():Void throw new IllegalStateException("TODO r7: spacingPlanAdjustmentsConsumeByTargetAndAnchor");
-    @:test public static function attachedInlineBoundariesRequireAlignmentAndRunOnlyWithAttachments():Void throw new IllegalStateException("TODO r7: attachedInlineBoundariesRequireAlignmentAndRunOnlyWithAttachments");
-    @:test public static function attachedInlineBoundaryAtLineEndConsumesTrailingGlue():Void throw new IllegalStateException("TODO r7: attachedInlineBoundaryAtLineEndConsumesTrailingGlue");
-    @:test public static function attachedInlineBoundaryAdjacentPunctuationHalvesTheVirtualGlue():Void throw new IllegalStateException("TODO r7: attachedInlineBoundaryAdjacentPunctuationHalvesTheVirtualGlue");
-    @:test public static function attachedInlineBoundaryBeforeAsciiPointMarkCollapsesLikeAdjacent():Void throw new IllegalStateException("TODO r7: attachedInlineBoundaryBeforeAsciiPointMarkCollapsesLikeAdjacent");
-    @:test public static function attachedInlineBoundarySkipsMandatoryBreakNeighbour():Void throw new IllegalStateException("TODO r7: attachedInlineBoundarySkipsMandatoryBreakNeighbour");
-    @:test public static function attachedInlineBoundaryWithoutGlueEmitsNoDecision():Void throw new IllegalStateException("TODO r7: attachedInlineBoundaryWithoutGlueEmitsNoDecision");
-    @:test public static function lineEdgeTrimConsumesHalfWidthAtEdgesAndSkipsEmptyInputs():Void throw new IllegalStateException("TODO r7: lineEdgeTrimConsumesHalfWidthAtEdgesAndSkipsEmptyInputs");
-    @:test public static function lineEdgeTrimConsumesCentredPunctuationOncePerLine():Void throw new IllegalStateException("TODO r7: lineEdgeTrimConsumesCentredPunctuationOncePerLine");
-    @:test public static function clusterIndexRangeFindCoveredClusters():Void throw new IllegalStateException("TODO r7: clusterIndexRangeFindCoveredClusters");
+
+@:test class PunctuationGeometryLedgerCoverageTest {
+    @:test public static function budgetsResolveAdvancesThroughRemainingGlue():Void throw new IllegalStateException("TODO r8: budgetsResolveAdvancesThroughRemainingGlue");
+    @:test public static function glueCapacitiesReportSidesAndPairing():Void throw new IllegalStateException("TODO r8: glueCapacitiesReportSidesAndPairing");
+    @:test public static function sideConsumptionIsCappedAndSkipsNonPositiveAmounts():Void throw new IllegalStateException("TODO r8: sideConsumptionIsCappedAndSkipsNonPositiveAmounts");
+    @:test public static function justificationDeltasAndStructuralChannelsFeedResolvedAdvance():Void throw new IllegalStateException("TODO r8: justificationDeltasAndStructuralChannelsFeedResolvedAdvance");
+    @:test public static function geometryWithoutBudgetFallsBackToBodyWidth():Void throw new IllegalStateException("TODO r8: geometryWithoutBudgetFallsBackToBodyWidth");
+    @:test public static function decisionInfoListsEveryGeometryWithBudgets():Void throw new IllegalStateException("TODO r8: decisionInfoListsEveryGeometryWithBudgets");
+    @:test public static function spacingPlanAdjustmentsConsumeByTargetAndAnchor():Void throw new IllegalStateException("TODO r8: spacingPlanAdjustmentsConsumeByTargetAndAnchor");
+    @:test public static function attachedInlineBoundariesRequireAlignmentAndRunOnlyWithAttachments():Void throw new IllegalStateException("TODO r8: attachedInlineBoundariesRequireAlignmentAndRunOnlyWithAttachments");
+    @:test public static function attachedInlineBoundaryAtLineEndConsumesTrailingGlue():Void throw new IllegalStateException("TODO r8: attachedInlineBoundaryAtLineEndConsumesTrailingGlue");
+    @:test public static function attachedInlineBoundaryAdjacentPunctuationHalvesTheVirtualGlue():Void throw new IllegalStateException("TODO r8: attachedInlineBoundaryAdjacentPunctuationHalvesTheVirtualGlue");
+    @:test public static function attachedInlineBoundaryBeforeAsciiPointMarkCollapsesLikeAdjacent():Void throw new IllegalStateException("TODO r8: attachedInlineBoundaryBeforeAsciiPointMarkCollapsesLikeAdjacent");
+    @:test public static function attachedInlineBoundarySkipsMandatoryBreakNeighbour():Void throw new IllegalStateException("TODO r8: attachedInlineBoundarySkipsMandatoryBreakNeighbour");
+    @:test public static function attachedInlineBoundaryWithoutGlueEmitsNoDecision():Void throw new IllegalStateException("TODO r8: attachedInlineBoundaryWithoutGlueEmitsNoDecision");
+    @:test public static function lineEdgeTrimConsumesHalfWidthAtEdgesAndSkipsEmptyInputs():Void throw new IllegalStateException("TODO r8: lineEdgeTrimConsumesHalfWidthAtEdgesAndSkipsEmptyInputs");
+    @:test public static function lineEdgeTrimConsumesCentredPunctuationOncePerLine():Void throw new IllegalStateException("TODO r8: lineEdgeTrimConsumesCentredPunctuationOncePerLine");
+    @:test public static function clusterIndexRangeFindCoveredClusters():Void throw new IllegalStateException("TODO r8: clusterIndexRangeFindCoveredClusters");
 }
